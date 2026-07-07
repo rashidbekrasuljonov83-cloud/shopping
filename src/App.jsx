c095b2029e30357e168716e2893a1aac8110c8ea
Rashidbek <rasishidbekrasuljonov83>
2026-07-07 13:46:02 +0300

    The footer is completely ready.

--- src/App.jsx
+++ src/App.jsx
@@ -1,10 +1,12 @@
 import Home from "./pages/Home";
 import Main from "./components/Main";
+import Foooter from "./components/Footer";
 function App() {
   return (
     <>
       <Main />
       <Home />
+      <Foooter />
     </>
   );
 }
